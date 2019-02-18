1e710537f7f8a93db30bb7ff90548f905bc534b3
Sebastian Lague <sebastlague@gmail.com>
2019-02-18 18:34:07 +0200

    Initial commit

---
+++ Assets/Scripts/HeightMapGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightMapGenerator : MonoBehaviour {
+    public int seed;
+    public bool randomizeSeed;
+
+    public int mapSize = 256;
+    public int numOctaves = 4;
+    public float heightMultiplier = 1;
+    public float persistence = .5f;
+    public float lacunarity = 2;
+    public float initialScale = 1;
+
+    public float[] Generate () {
+        var map = new float[mapSize * mapSize];
+        seed = (randomizeSeed) ? Random.Range (-10000, 10000) : seed;
+        var prng = new System.Random (seed);
+
+        Vector2[] offsets = new Vector2[numOctaves];
+        for (int i = 0; i < numOctaves; i++) {
+            offsets[i] = new Vector2 (prng.Next (-1000, 1000), prng.Next (-1000, 1000));
+        }
+
+        for (int y = 0; y < mapSize; y++) {
+            for (int x = 0; x < mapSize; x++) {
+
+                float noiseValue = 0;
+                float scale = initialScale;
+                float weight = 1;
+                for (int i = 0; i < numOctaves; i++) {
+                    Vector2 p = offsets[i] + new Vector2(x/(float)mapSize,y/(float)mapSize) * scale;
+                    noiseValue += Mathf.PerlinNoise(p.x,p.y) * weight;
+                    weight *= persistence;
+                    scale *= lacunarity;
+                }
+                map[y*mapSize+x] = noiseValue;
+            }
+        }
+
+        return map;
+    }
+}
---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour {
+
+    public float scale = 1;
+    public float elevationScale = 1;
+
+    [ContextMenu ("Run")]
+    public void Run () {
+        GenerateMesh (FindObjectOfType<HeightMapGenerator> ().Generate ());
+    }
+
+    void Update () {
+        GenerateMesh (FindObjectOfType<HeightMapGenerator> ().Generate ());
+    }
+
+    public void GenerateMesh (float[] map) {
+        int width = map.GetLength (0);
+        int height = map.GetLength (1);
+
+        Vector3[] verts = new Vector3[width * height];
+        int[] triangles = new int[(width - 1) * (height - 1) * 6];
+        int t = 0;
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int i = y * width + x;
+
+                Vector2 percent = new Vector2 (x / (width - 1f), y / (height - 1f));
+                Vector3 pos = new Vector3 (percent.x * 2 - 1, 0, percent.y * 2 - 1) * scale;
+                pos += Vector3.up * map[i] * elevationScale;
+                verts[i] = pos;
+
+                if (x != width - 1 && y != height - 1) {
+
+                    triangles[t + 0] = i + width;
+                    triangles[t + 1] = i + width + 1;
+                    triangles[t + 2] = i;
+
+                    triangles[t + 3] = i + width + 1;
+                    triangles[t + 4] = i + 1;
+                    triangles[t + 5] = i;
+                    t += 6;
+                }
+            }
+        }
+
+        Mesh mesh = new Mesh () {
+            vertices = verts,
+            triangles = triangles
+        };
+        mesh.RecalculateNormals ();
+
+        GetComponent<MeshFilter> ().mesh = mesh;
+
+    }
+}
